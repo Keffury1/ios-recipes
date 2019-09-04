d8e1c3051b2ebbacfc915734ef731ae5fbf6fa9e
Robert Keffury <bobby.keffury@me.com>
2019-09-04 13:54:13 -0700

    Finished Storyboard and Detail View Controller

---
+++ Recipes/Recipes/View Controllers/MainViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainViewController.swift
+//  Recipes
+//
+//  Created by Bobby Keffury on 9/4/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func searchTextField(_ sender: Any) {
+    }
+    
+
+}
---
+++ Recipes/Recipes/View Controllers/RecipeDetailViewController.swift
@@ -0,0 +1,56 @@
+//
+//  RecipeDetailViewController.swift
+//  Recipes
+//
+//  Created by Bobby Keffury on 9/4/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var recipeTextView: UITextView!
+    
+    // MARK: - Properties
+    
+    var recipe: Recipe? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateViews()
+        
+    }
+    
+    // MARK: - Methods
+    
+    func updateViews() {
+        guard let recipe = recipe else {
+            self.isViewLoaded
+            return
+        }
+        titleLabel.text = recipe.name
+        recipeTextView.text = recipe.instructions
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
